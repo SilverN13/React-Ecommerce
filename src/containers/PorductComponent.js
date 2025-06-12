2ace56e56a8e523960778910f35fd9afe2535632
SilverN13 <silvamin802@gmail.com>
2025-06-12 23:24:35 +0300

    end code

--- src/containers/PorductComponent.js
+++ src/containers/PorductComponent.js
@@ -1,27 +1,32 @@
 import React from "react";
+import {Link, link} from "react-router-dom";
 import {useSelector} from "react-redux";
 
 const ProductComponent = ()=> {
     const products= useSelector((state)=> state.allProducts.products);
-    const {id,title} = products[0];
-    return(
-        <div className="four column wide">
+    const renderList = products.map((product)=>{
+        const {id,title,image,price,category} = product;
+        return(
+            <div className="four column wide" key={id}>
+              <Link to={`/product/${id}`}>
             <div className="ui link cards">
-                <div className="card">
-                <div className="image">
-                <div className="content">
-                    <div className="header">
-                    {title}
-                    </div>
-
-                </div>
-                </div>
-                </div>
-
+              <div className="card">
+                  <div className="image">
+                    <img src={image} alt={image}/>
+                  </div>
+                  <div className="content">
+                      <div className="header">{title}</div>
+                      <div className="meta price">$ {price}</div>
+                      <div className="meta ">{category}</div>
+                  </div>
+              </div>
             </div>
-            
-        </div>
-    )
+            </Link>
+          </div>
+        )
+    })
+    return<>{renderList}</>
+    
 }
 
 export default ProductComponent;
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,21 +1,24 @@
 import { ActionTypes } from "../contants/action-types";
 
 const initialState = {
-  products: [
-    {
-      id: 1,
-      title: "Mohammed",
-      category: "programmer",
-    },
-  ],
+  products: [],
 };
 
-export const productReducer = (state =initialState, { type, payload }) => {
+export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
-      return state;
+      return { ...state, products: payload };
 
     default:
       return state;
   }
 };
+
+export const selectedProductReducer = (state ={}, {type,payload})=>{
+  switch(type){
+    case ActionTypes.SELECTED_PRODUCTS:
+      return{...state,...payload};
+      default:
+       return state;
+  }
+}
